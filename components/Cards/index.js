986991452879979175ce93148b5f238b9038b1e6
Carla Marvin <carlamarvin78@gmail.com>
2019-07-12 15:29:00 -0400

    Task 3

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,35 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+
+
+
+function createCard(data){
+    const card=document.createElement('div')
+    const headline=document.createElement('div');
+    const author=document.createElement('div');
+    const imgContainer=document.createElement('div');
+    const img=document.createElement('img');
+    const span=document.createElement('span');
+
+    card.classList.add(card);
+    headline.classList.add('headline');
+    author.classList.add('headline');
+    imgContainer.classList.add('img-container');
+    img.classList.add('img');
+    span.classList.add('span');
+
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(imgContainer);
+    imgContainer.appendChild(img);
+    author.appendChild(span);
+
+    headline.textContent=data.headline;
+    img.setAttribute('src', data.authorPhoto);
+    span.textContent=`By ${authorName}`;
+
+return card;
+}
