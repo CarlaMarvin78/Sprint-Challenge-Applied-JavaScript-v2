dc8356505493478147e3f5d7aca0037ae661ead7
Carla Marvin <carlamarvin78@gmail.com>
2019-07-12 12:29:50 -0400

    Index.js complete

--- components/Header/index.js
+++ components/Header/index.js
@@ -5,8 +5,28 @@
 //  <div class="header">
 //    <span class="date">MARCH 28, 2019</span>
 //    <h1>Lambda Times</h1>
-//    <span class="temp">98°</span>
+//    <span class="temp"></span>
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    const header=createElement('div');
+    const date=createElement('span');
+    const title=createElement('h1');
+    const temp=createElem98°ent('span');
+
+    header.appendChild(date);
+    header.appendChild(title);
+    header.appendChild(temp);
+
+    header.classList.add('header');
+    date.classList.add('date');
+    title.classList.add('title');
+    temp.classList.add('temp');
+
+    date.textContent=('March 28, 2019');
+    title.textContent=('Lambda Times')
+    temp.textContent=('98°');
+
+    return header;      
+}
